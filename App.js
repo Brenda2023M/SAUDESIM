6949fb17d980f76a939fdee5add9bdc89ee50b3e
unknown <Brenda2023M>
2024-04-11 08:49:38 -0300

    formulario 3B1 Lado A

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 
 import { StyleSheet, Text, View } from 'react-native';
 import Title from './Src/components/Title';
+import Main from './SRC/Main';
 
 export default function App() {
   return (
     <View style={styles.container}>
      <Title/>
+     <Main/>
     </View>
 
   );

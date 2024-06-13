c21de2b02ef696b35f7338028566e270922c337c
Brenda2023M <brenda.nascimento15@etec.sp.gov.br>
2024-06-13 08:52:29 -0300

    Title

---
+++ SRC/components/Title/style.js
@@ -0,0 +1,17 @@
+import {StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    BoxTitle: {
+        alignItems: "center",
+        justifyContent: "center",
+        padding: 10,
+    },
+
+    textTitle:{
+        color: "80CDC1",
+        fontSize: 32,
+        fontWeight: "bold",
+    },
+
+});
+export default styles 
